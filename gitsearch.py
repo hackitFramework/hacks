03106ee10443005866d98049b06756b55b35f94f
chansen424 <chansen424@yahoo.com>
2018-11-19 07:41:53 +0000

    Full Search now works.

--- gitsearch.py
+++ gitsearch.py
@@ -5,36 +5,26 @@
 
 # Git got grabed
 
-import os
+import os, pygithub3
 #import requests
-import pygithub3
 #r = requests.get("https://api.github.com/users/hsamuelson/repos")
 #print(r.text)
 
-
-
 gh = None
+
 def gather_clone_urls(organization, no_forks=True):
     all_repos = gh.repos.list(user=organization).all()
-    for repo in all_repos:
-
-        # Don't print the urls for repos that are forks.
-        if no_forks and repo.fork:
-            continue
-
-        yield repo.clone_url
+    return all_repos
 
 def fullSearch(userName):
-	
     # First get repo list 
-    
-    gh = pygithub3.Github()
     repoList = gather_clone_urls(userName)
     
     for repo in repoList:
         # Make os call to run bash script
-        repoName = repo.rsplit('/',1)[-1]
-        command = "bash yomam.sh " + repo + " " + repoName
+        repo = str(repo)
+        repoName = repo[repo.find("(")+1:repo.find(")")]
+        command = "bash yomam.sh https://github.com/" +  userName + "/" + repoName
         os.system(command)
 
 
@@ -55,6 +45,7 @@ print("Select 1: for individual repo search")
 menuSelect = raw_input("Select Search> ")
 userName = raw_input("Target User> ")
 if(int(menuSelect) == 0):
+    gh = pygithub3.Github()
     fullSearch(userName)
 else:
     repoName = raw_input("Target Repo> ")
