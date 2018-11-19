2dfd18faf008be36820d7c3ccf3d09d6e603571b
hsamuelson <hsamuelson@eastsideprep.org>
2018-11-19 11:25:49 -0500

    webgrab changes

--- gitgrab/gitgrab.py
+++ gitgrab/gitgrab.py
@@ -1,6 +1,6 @@
 #
 # Git Grab 2018
-# 
+#
 # Henry Samuelson, Christopher Hansen
 
 # Git got grabed
@@ -17,9 +17,9 @@ def gather_clone_urls(organization, no_forks=True):
     return all_repos
 
 def fullSearch(userName, usrSearch):
-    # First get repo list 
+    # First get repo list
     repoList = gather_clone_urls(userName)
-    
+
     for repo in repoList:
         # Make os call to run bash script
         repo = str(repo)
@@ -29,6 +29,9 @@ def fullSearch(userName, usrSearch):
 
 def individualSearch(userName, repoName, usrSearch):
     command = "bash yomam.sh https://github.com/" + userName + "/" + repoName + " " + repoName + " " + usrSearch
+    os.system(command)
+def webGrab(webUrl, query):
+    command = "bash httrackDump.sh " + webUrl + " " + query
     os.system(command)
 #
 ############# MAIN PROGRAM /UI ######
@@ -49,18 +52,26 @@ print("Search git repos for key text")
 print("*")
 print("")
 print("")
-print("Select 0: full search")
-print("Select 1: for individual repo search")
+print("Select 0: webGrab")
+print("Select 1: gitGrab")
+initialSelect = raw_input("Select> ")
+if(int(initialSelect) == 0):
+    url = raw_input("Target url> ")
+    query = raw_input("Query Text> ")
+    webGrab(url, query)
+if(int(initialSelect) == 1):
+    print("Select 0: full search")
+    print("Select 1: for individual repo search")
 
-menuSelect = raw_input("Select Search> ")
-selectText = raw_input("Search phrase> ")
-userName = raw_input("Target User> ")
+    menuSelect = raw_input("Select Search> ")
+    selectText = raw_input("Search phrase> ")
+    userName = raw_input("Target User> ")
 
-if(int(menuSelect) == 0):
-    gh = pygithub3.Github()
-    print("Results:")
-    fullSearch(userName, selectText)
-else:
-    repoName = raw_input("Target Repo> ")
-    print("Results:")
-    individualSearch(userName, repoName, selectText)
+    if(int(menuSelect) == 0):
+        gh = pygithub3.Github()
+        print("Results:")
+        fullSearch(userName, selectText)
+    else:
+        repoName = raw_input("Target Repo> ")
+        print("Results:")
+        individualSearch(userName, repoName, selectText)
