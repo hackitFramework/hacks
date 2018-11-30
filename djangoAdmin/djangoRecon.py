4471fb6f44f6ff9e816ccb632fdb112b57e9c450
Henry Samuelson <hsamuelson@users.noreply.github.com>
2018-11-30 04:11:09 -0500

    processingHTML request body

--- djangoAdmin/djangoRecon.py
+++ djangoAdmin/djangoRecon.py
@@ -6,7 +6,7 @@
 # This is built to work assuming server is running on localhost
 
 import requests
-import system
+from bs4 import BeautifulSoup
 
 def ui():
     #system('cls')
@@ -14,6 +14,7 @@ def ui():
     input1 = str(raw_input("Target Admin page url (str)> "))
     requestPage(input1)
     pass
+
 
 def requestPage(url = 0):#url):
 
@@ -31,7 +32,41 @@ def requestPage(url = 0):#url):
     r = client.post('http://localhost:8000/admin/login/?next=/admin/', data = payload)
     #r = r.json()
     print(r.text)
+    file = open("outputer.html", "w")
+    file.write(str(r.text))
     return r.text
 
+def parseHTML(html_doc, file = False):
+    """
+    Takes html str
+
+    Returns: str, all text inside of html document
+    """
+    assert type(html_doc) == str
+
+    if(file == True): #is a file name
+        contents = open(html_doc, 'r')
+        html_doc = contents1.read()
+        contents.close()
+    return BeautifulSoup(html_doc, 'html.parser')
+    #fileContents = BeautifulSoup(html_doc, 'html.parser')
+    #return fileContents.get_text()
+
+def textOfInterest(soup):
+    contents = []
+    for line in soup.get_text().split('\n'):
+        if(line != ''):
+            temp = line.split()
+            temp =  str(temp)[1:len(str(temp))-1]
+            contents.append(temp)
+
+    fContent = [] #this will be in an ordered method:  [header-1, contents-1, header-n,contents-n]
+    for i in range(1, len(contents)):
+        if(contents[i].isupper()):
+            # Add this line and line below it.
+            fContent.append(contents[i])
+            fContent.append(contents[i+1])
+
+    return fContent
+
 requestPage()
-#ui()
---
+++ djangoAdmin/htmlReader.py
@@ -0,0 +1,46 @@
+#
+# html reader
+#
+# Henry Samuelson 11/30/18
+
+#THIS IS A TEST FILE TO AVLOID REPULLING HTML EVERY TIME WE WANT TO TEST NEW
+# HTML ANALYSIS
+html_doc1 = open("outputer.html", 'r')
+html_doc = html_doc1.read()
+html_doc1.close()
+
+# html_doc = """
+# <html><head><title>The Dormouse's story</title></head>
+# <body>
+# <p class="title"><b>The Dormouse's story</b></p>
+#
+# <p class="story">Once upon a time there were three little sisters; and their names were
+# <a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+# <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+# <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+# and they lived at the bottom of a well.</p>
+#
+# <p class="story">...</p>
+# """
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+#print(soup.prettify())
+#Removes all html
+
+print(soup.get_text("/n"))
+
+contents = []
+for line in soup.get_text().split('\n'):
+    if(line != ''):
+        temp = line.split()
+        temp =  str(temp)[1:len(str(temp))-1]
+        contents.append(temp)
+
+fContent = []
+for i in range(1, len(contents)):
+    if(contents[i].isupper()):
+        # Add this line and line below it.
+        fContent.append(contents[i])
+        fContent.append(contents[i+1])
