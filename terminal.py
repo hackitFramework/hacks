94699202910770047ce352a5642073c62a296010
hsamuelson <hsamuelson@eastsideprep.org>
2019-01-20 14:35:41 -0500

    working on base64 forcing incodingin in go

---
+++ terminal.py
@@ -0,0 +1,9 @@
+import os
+counter = 1
+os.system("python")
+while(True):
+    print(counter)
+    #os.system("python")
+    os.system("import os")
+    os.system("os.system('python')")
+    counter = counter + 1
