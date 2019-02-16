21d0329799f2edd71597270986edbfa33ab6fac3
chansen424 <chansen424@yahoo.com>
2019-02-16 16:34:03 -0500

    Screenshot
    Screenshot release build:
    Completely invisible (use .exe)
    App.xaml.cs to show code (no other purpose)

---
+++ TrojanNotifier/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Drawing;
+
+namespace Screenshot
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Bitmap memoryImage;
+            memoryImage = new Bitmap(1920, 1080);
+            System.Drawing.Size s = new System.Drawing.Size(memoryImage.Width, memoryImage.Height);
+
+            // Getting screen
+            Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+            memoryGraphics.CopyFromScreen(0, 0, 0, 0, s);
+
+            // Save image 
+            string str = "";
+            try
+            {
+                str = string.Format(@".\screenshot.png");
+            }
+            catch (Exception er)
+            {
+                Console.WriteLine("Sorry, there was an error: " + er.Message);
+                Console.WriteLine();
+            }
+            memoryImage.Save(str);
+
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
